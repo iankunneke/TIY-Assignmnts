9474bc0c08d51256ddd5b0d864e6111b5e49792a
iankunneke <iankunneke@live.com>
2015-06-23 14:36:19 -0400

    finished part of my Age Calculator

---
+++ 05--Age-Calculator/AgeCalculator/AgeCalculator/AgeViewController.m
@@ -0,0 +1,42 @@
+//
+//  AgeViewController.m
+//  AgeCalculator
+//
+//  Created by ian kunneke on 6/22/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import "AgeViewController.h"
+
+@interface AgeViewController ()
+
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+
+
+@end
+
+@implementation AgeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+
+@end
--- 05--Age-Calculator/AgeCalculator/AgeCalculator/ViewController.m
+++ 05--Age-Calculator/AgeCalculator/AgeCalculator/ViewController.m
@@ -7,21 +7,57 @@
 //
 
 #import "ViewController.h"
+#import "AgeViewController.h"
 
 @interface ViewController ()
+{
+    
+    NSDateFormatter *dateFormatter;
+    NSInteger *yourAge;
+
+}
+
+
+@property (weak, nonatomic) IBOutlet UILabel *currentDayLabel;
+@property (weak, nonatomic) IBOutlet UILabel *yourBirthdayLabel;
+@property (weak, nonatomic) IBOutlet UILabel *yourAgeIsLabel;
+
+
+
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
+    
+    self.title = @"Age Calculator";
+    
+    dateFormatter = [[NSDateFormatter alloc] init];
+    
+    
+    NSString *formatString = [NSDateFormatter dateFormatFromTemplate:@"MMMddyyyy"
+                                                             options:0
+                                                              locale:[NSLocale currentLocale]];
+    
+    dateFormatter.dateFormat = formatString;
+    
+    
+    self.currentDayLabel.text = [dateFormatter stringFromDate:[NSDate date]];
+    
+    
+
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    
+    
 }
 
 @end
