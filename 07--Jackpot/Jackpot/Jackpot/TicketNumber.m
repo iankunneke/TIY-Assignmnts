3cf236fb645f342709d2702696a11f8ce9d9d3ed
iankunneke <iankunneke@live.com>
2015-06-24 15:21:07 -0400

    Finished some of Jackpot

---
+++ 07--Jackpot/Jackpot/Jackpot/TicketNumber.m
@@ -0,0 +1,46 @@
+//
+//  TicketNumber.m
+//  Jackpot
+//
+//  Created by ian kunneke on 6/23/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import "TicketNumber.h"
+
+@interface TicketNumber ()
+
+
+@end
+
+@implementation TicketNumber
+
++ (instancetype)ticketUsingPicker
+{
+    TicketNumber *aTicket = [[TicketNumber alloc] init];
+    for (int i=0; i < 6; i++)
+    {
+        [aTicket createNumbers];
+    }
+    return aTicket;
+}
+
+- (instancetype) init
+{
+    self = [super init];
+    if (self)
+    {
+        _choices = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+
+- (void) createNumbers
+{
+    int aPick = arc4random() % 53 +1;
+    NSNumber *anInt = [NSNumber numberWithInt:aPick];
+    [_choices addObject:anInt];
+}
+
+@end
