128dfd51460db55ea21eb011ce96fbb6809e86c9
iankunneke <iankunneke@live.com>
2015-06-24 15:25:20 -0400

    initial commit for "Human"

---
+++ Human/Human/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Human
+//
+//  Created by ian kunneke on 6/24/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
