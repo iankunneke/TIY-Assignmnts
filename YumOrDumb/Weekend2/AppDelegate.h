9fce36a9bfda4e6842d57eeb3cd68cba609c004b
iankunneke <iankunneke@live.com>
2015-06-26 10:12:25 -0400

    initial commit for homework 9

---
+++ YumOrDumb/Weekend2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weekend2
+//
+//  Created by ian kunneke on 6/25/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YumOrDumb/Weekend2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weekend2
+//
+//  Created by ian kunneke on 6/25/15.
+//  Copyright (c) 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
